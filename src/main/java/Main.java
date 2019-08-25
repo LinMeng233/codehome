a2f3e5601dae729a49772628d2a2647963cdd1db
LinMeng233 <291157220@qq.com>
2019-08-25 17:06:26 +0800

    第一次push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) throws Exception{
-        String str="鸡你太美";
-        String str2=new String(str.getBytes(),"UTF-8");
-        String a="1";
+    public static void main(String[] args) throws Exception {
+         int a=1;
     }
 
+
 }
+
