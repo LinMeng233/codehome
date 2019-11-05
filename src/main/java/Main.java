1c911dee3d23797a6ed03e6414c01d4f51a45b5e
291157220@qq.com <291157220@qq.com>
2019-11-05 22:14:20 +0800

    拦截器

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import jdk.nashorn.internal.objects.annotations.Property;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         
---
+++ src/main/java/com/huanyupiaoling/codehome/interceptor/HanderInterceptorOne.java
@@ -0,0 +1,26 @@
+package com.huanyupiaoling.codehome.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HanderInterceptorOne implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("sb1");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("sb2");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("sb3");
+    }
+}
